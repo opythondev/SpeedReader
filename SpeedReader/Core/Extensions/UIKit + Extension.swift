4e4a51d8a908715999197110f477affad138b6fe
opythondev <115350236+opythondev@users.noreply.github.com>
2023-01-06 09:35:22 +0300

    upd []: extension to UIViewController, setup ui method createEmptyTableView

---
+++ SpeedReader/Core/Extensions/UIKit + Extension.swift
@@ -0,0 +1,12 @@
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func createEmptyTableView() -> UITableView {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }
+}
