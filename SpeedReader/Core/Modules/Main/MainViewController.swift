572876006b6ff13d56b80b945263f40bb69aa205
opythondev <115350236+opythondev@users.noreply.github.com>
2023-03-15 17:45:07 +0300

    main VC + xib prepared

---
+++ SpeedReader/Core/Modules/Main/MainViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
