3a2d1cd10d463a3b3c2788be16fbe9f344516855
opythondev <115350236+opythondev@users.noreply.github.com>
2023-03-27 15:51:48 +0300

    upd[Main Module]: prepared view for testing

--- SpeedReader/Core/Modules/Main/MainViewController.swift
+++ SpeedReader/Core/Modules/Main/MainViewController.swift
@@ -2,8 +2,6 @@ import UIKit
 
 class MainViewController: UIViewController {
     
-    @IBOutlet weak var tableView: UITableView!
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
